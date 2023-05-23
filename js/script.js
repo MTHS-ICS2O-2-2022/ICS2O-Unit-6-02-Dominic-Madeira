e60014b5f11d7534e1e9e5cfe6c2e0c35195f56b
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-23 16:58:58 +0000

    first edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Dominic M. All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Dominic M.
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-02-Dominic-Madeira/sw.js", {
+    scope: "/ICS2O-Unit-6-02-Dominic-Madeira/",
+  })
+}
+
+function myButtonClicked() {
+  alert("Calculation Failed!!")
+}
