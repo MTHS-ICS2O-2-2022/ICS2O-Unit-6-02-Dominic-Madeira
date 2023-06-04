408c9e721a131f562d9c00bec1c7e76378a984e9
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-06-04 00:36:01 +0000

    removed duplicate file

--- script.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2023 Dominic M. All rights reserved
-//
-// Created by: Dominic M.
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
-
-function myButtonClicked() {
-  alert("Calculation Failed!!")
-}
